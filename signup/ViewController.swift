50cef68ec1a05e0c3f13febfb844b7d97198ab9c
doug <doug@getweave.com>
2016-05-22 16:49:00 -0600

    create user programmatically

--- signup/ViewController.swift
+++ signup/ViewController.swift
@@ -7,19 +7,19 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let email = "test@test.com"
+        let password = "test123"
+        FIRAuth.auth()?.createUserWithEmail(email, password: password) { (user, error) in
+            print("User: \(user)")
+            print("Error: \(error)")
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
